e151a67ec9ad60f680b7d0542dab213d0dc7fce0
shadowfax <unaputacuenta@hotmail.com>
2012-04-19 20:04:29 +0200

    GUI Layout

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -6,7 +6,11 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 	{
 		$this->bootstrap('view');
         $view = $this->getResource('view');
+        $view->setEncoding('UTF-8');
         $view->doctype('XHTML1_STRICT');
+        $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8');
+        $view->headMeta()->appendHttpEquiv('Author', 'Juan Pedro Gonzalez Gutierrez');
+        $view->headMeta()->appendHttpEquiv('Copyright', '(c) Juan Pedro Gonzalez Gutierrez');
         
         $this->bootstrap('frontController');
         $view->headLink()->appendStylesheet($view->baseUrl('/css/layout.css'));
